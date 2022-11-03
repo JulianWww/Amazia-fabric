596d04c9b1d0f49b80e94a489e8cc176b696d8f3
JulianWww <Julian.wandhoven@gmail.com>
2022-11-03 23:39:49 +0100

    added toolsmith trades

--- src/main/java/net/denanu/amazia/economy/EconomyFactory.java
+++ src/main/java/net/denanu/amazia/economy/EconomyFactory.java
@@ -108,6 +108,45 @@ public class EconomyFactory {
 			.add(Enchantments.MENDING, 					0.001f)
 			.add(Enchantments.VANISHING_CURSE, 			0.2f);
 	
+	public final static EnchantmentModifier AXE_ENCHANTMENT_MODIFIER = new EnchantmentModifier(0.3f, 1.0f)
+			.add(Enchantments.FORTUNE,					1.0f)
+			.add(Enchantments.SILK_TOUCH,				1.0f)
+			.add(Enchantments.EFFICIENCY,				1.0f)
+			.add(Enchantments.UNBREAKING,				1.0f)
+			.add(Enchantments.SHARPNESS,				1.0f)
+			.add(Enchantments.SMITE,					1.0f)
+			.add(Enchantments.BANE_OF_ARTHROPODS,		1.0f)
+			.add(Enchantments.FIRE_ASPECT,				1.0f)
+			.add(Enchantments.LOOTING,					1.0f)
+			.add(Enchantments.KNOCKBACK,				1.0f)
+			.add(Enchantments.SWEEPING,					1.0f)
+			.add(Enchantments.MENDING,					0.001f)
+			.add(Enchantments.VANISHING_CURSE,			0.2f);
+	
+	public final static EnchantmentModifier PICKAXE_ENCHANTMENT_MODIFIER = new EnchantmentModifier(0.2f, 1.0f)
+			.add(Enchantments.EFFICIENCY,				5.0f)
+			.add(Enchantments.SILK_TOUCH,				1.0f)
+			.add(Enchantments.FORTUNE,					1.0f)
+			.add(Enchantments.UNBREAKING,				1.0f)
+			.add(Enchantments.MENDING,					0.001f)
+			.add(Enchantments.VANISHING_CURSE,			0.2f);
+	
+	public final static EnchantmentModifier SHOVEL_ENCHANTMENT_MODIFIER = new EnchantmentModifier(0.2f, 1.0f)
+			.add(Enchantments.EFFICIENCY,				5.0f)
+			.add(Enchantments.SILK_TOUCH,				1.0f)
+			.add(Enchantments.FORTUNE,					1.0f)
+			.add(Enchantments.UNBREAKING,				1.0f)
+			.add(Enchantments.MENDING,					0.001f)
+			.add(Enchantments.VANISHING_CURSE,			0.2f);
+	
+	public final static EnchantmentModifier HOE_ENCHANTMENT_MODIFIER = new EnchantmentModifier(0.2f, 1.0f)
+			.add(Enchantments.EFFICIENCY,				5.0f)
+			.add(Enchantments.SILK_TOUCH,				1.0f)
+			.add(Enchantments.FORTUNE,					1.0f)
+			.add(Enchantments.UNBREAKING,				1.0f)
+			.add(Enchantments.MENDING,					0.001f)
+			.add(Enchantments.VANISHING_CURSE,			0.2f);
+	
 	public final static EnchantmentModifier ENCHANTED_BOOK_MODIFIER = new EnchantmentModifier(0.3f, 1.0f)
 			.add(Enchantments.PROTECTION, 				1.0f)
 			.add(Enchantments.FIRE_PROTECTION, 			1.0f)
@@ -264,24 +303,24 @@ public class EconomyFactory {
 	public final static RandomnessFactory<Integer> DIE_RANDOMNESS_FACTORY = new ConstrainedGaussianRandom(16, 4, 64, 1);
 	
 	 																								 // value       volatility  return rate | stackSize generator							professions
-	public final static BaseItemEconomy COAL = 					register(Items.COAL, 					0.0666f, 	0.0001f, 	0.01f,	new ConstrainedGaussianRandom(20f, 8f, 64, 1), 	ImmutableSet.of("armorer", "butcher", "fischerman"));
+	public final static BaseItemEconomy COAL = 					register(Items.COAL, 					0.0666f, 	0.0001f, 	0.01f,	new ConstrainedGaussianRandom(20f, 8f, 64, 1), 	ImmutableSet.of("armorer", "butcher", "fischerman", "toolsmith"));
 	public final static BaseItemEconomy IRON_HELMET = 			register(Items.IRON_HELMET, 			5, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(HELMET_ENCHANTMENT_MODIFIER);
 	public final static BaseItemEconomy IRON_CHESTPLATE = 		register(Items.IRON_CHESTPLATE, 		9, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(CHESTPLATE_ENCHANTMENT_MODIFIER);
 	public final static BaseItemEconomy IRON_LEGGINS =			register(Items.IRON_LEGGINGS, 			7, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(LEGGINS_ENCHANTMENT_MODIFIER);
 	public final static BaseItemEconomy IRON_BOOTS = 			register(Items.IRON_BOOTS, 				4, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(BOOTS_ENCHANTMENT_MODIFIER);
-	public final static BaseItemEconomy IRON_INGOT = 			register(Items.IRON_INGOT, 				0.25f,		0.001f, 	0.01f, 	new LinearRandom(1, 64), 						ImmutableSet.of("armorer"));
-	public final static BaseItemEconomy BELL = 					register(Items.BELL, 					36, 		2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer"));
+	public final static BaseItemEconomy IRON_INGOT = 			register(Items.IRON_INGOT, 				0.25f,		0.001f, 	0.01f, 	new LinearRandom(1, 64), 						ImmutableSet.of("armorer", "toolsmith"));
+	public final static BaseItemEconomy BELL = 					register(Items.BELL, 					36, 		2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer", "toolsmith"));
 	public final static BaseItemEconomy LAVA_BUCKET =			register(Items.LAVA_BUCKET, 			1, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer"));
-	public final static BaseItemEconomy DIAMOND = 				register(Items.DIAMOND, 				10,			0.01f, 		0.001f, new ConstrainedGaussianRandom(16f, 4f, 32, 1), 	ImmutableSet.of("armorer"));
-	public final static BaseItemEconomy CHAINMAIL_HELMET = 		register(Items.CHAINMAIL_HELMET,		1, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(HELMET_ENCHANTMENT_MODIFIER);
-	public final static BaseItemEconomy CHAINMAIL_CHESTPLATE = 	register(Items.CHAINMAIL_CHESTPLATE,	4, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(CHESTPLATE_ENCHANTMENT_MODIFIER);
-	public final static BaseItemEconomy CHAINMAIL_LEGGINGS = 	register(Items.CHAINMAIL_LEGGINGS, 		3, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(LEGGINS_ENCHANTMENT_MODIFIER);
-	public final static BaseItemEconomy CHAINMAIL_BOOTS = 		register(Items.CHAINMAIL_BOOTS, 		1, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(BOOTS_ENCHANTMENT_MODIFIER);
-	public final static BaseItemEconomy SHIELD = 				register(Items.SHIELD, 					5, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(SHIELD_ENCHANTMENT_MODIFIER);
-	public final static BaseItemEconomy DIAMOND_HELMET = 		register(Items.DIAMOND_HELMET, 			27, 		4f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(HELMET_ENCHANTMENT_MODIFIER);
-	public final static BaseItemEconomy DIAMOND_CHESTPLATE = 	register(Items.DIAMOND_CHESTPLATE, 		35, 		4f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(CHESTPLATE_ENCHANTMENT_MODIFIER);
-	public final static BaseItemEconomy DIAMOND_LEGGINGS = 		register(Items.DIAMOND_LEGGINGS, 		33, 		4f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(LEGGINS_ENCHANTMENT_MODIFIER);
-	public final static BaseItemEconomy DIAMOND_BOOTS = 		register(Items.DIAMOND_BOOTS, 			27, 		4f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(BOOTS_ENCHANTMENT_MODIFIER);
+	public final static BaseItemEconomy DIAMOND = 				register(Items.DIAMOND, 				10,			0.01f, 		0.001f, new ConstrainedGaussianRandom(16f, 4f, 32, 1), 	ImmutableSet.of("armorer", "toolsmith"));
+	public final static BaseItemEconomy CHAINMAIL_HELMET = 		register(Items.CHAINMAIL_HELMET,		1, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(HELMET_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy CHAINMAIL_CHESTPLATE = 	register(Items.CHAINMAIL_CHESTPLATE,	4, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(CHESTPLATE_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy CHAINMAIL_LEGGINGS = 	register(Items.CHAINMAIL_LEGGINGS, 		3, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(LEGGINS_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy CHAINMAIL_BOOTS = 		register(Items.CHAINMAIL_BOOTS, 		1, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(BOOTS_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy SHIELD = 				register(Items.SHIELD, 					5, 			2f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(SHIELD_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy DIAMOND_HELMET = 		register(Items.DIAMOND_HELMET, 			27, 		4f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(HELMET_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy DIAMOND_CHESTPLATE = 	register(Items.DIAMOND_CHESTPLATE, 		35, 		4f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(CHESTPLATE_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy DIAMOND_LEGGINGS = 		register(Items.DIAMOND_LEGGINGS, 		33, 		4f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(LEGGINS_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy DIAMOND_BOOTS = 		register(Items.DIAMOND_BOOTS, 			27, 		4f, 		0.001f, new ConstantValue<Integer>(1), 					ImmutableSet.of("armorer")).modify(BOOTS_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
 	
 	public final static BaseItemEconomy CHICKEN = 				register(Items.CHICKEN, 				0.07f, 		1f,			0.001f, new ConstrainedGaussianRandom(32, 16, 64, 1),	ImmutableSet.of("butcher"));
 	public final static BaseItemEconomy PORKCHOP = 				register(Items.PORKCHOP, 				0.14f, 		1f,			0.001f, new ConstrainedGaussianRandom(32, 16, 64, 1),	ImmutableSet.of("butcher"));
@@ -342,7 +381,7 @@ public class EconomyFactory {
 	public final static BaseItemEconomy COOKED_SALMON = 		ItemCompundEconomy.register(Items.COOKED_SALMON, ImmutableSet.of(ImmutablePair.of(COD, 1f), ImmutablePair.of(COAL, 0.2f)), new ConstrainedGaussianRandom(32, 8, 64, 1), 	ImmutableSet.of("fischerman"));
 
 	public final static BaseItemEconomy STICK = 				register(Items.STICK,					0.03f, 		0.01f,		0.001f, new ConstrainedGaussianRandom(32, 8, 64, 1),	ImmutableSet.of("fletcher"));
-	public final static BaseItemEconomy FLINT = 				register(Items.FLINT,					0.03f, 		0.01f,		0.001f, new ConstrainedGaussianRandom(32, 8, 64, 1),	ImmutableSet.of("fletcher", "leatherworker"));
+	public final static BaseItemEconomy FLINT = 				register(Items.FLINT,					0.03f, 		0.01f,		0.001f, new ConstrainedGaussianRandom(32, 8, 64, 1),	ImmutableSet.of("fletcher", "leatherworker", "toolsmith"));
 	public final static BaseItemEconomy FEATHER = 				register(Items.FEATHER,					0.1f, 		0.01f,		0.001f, new ConstrainedGaussianRandom(32, 8, 64, 1),	ImmutableSet.of("fletcher"));
 	public final static BaseItemEconomy BOW = 					register(Items.BOW,						2f, 		1f,			0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("fletcher")).modify(BOW_ENCHANTMENT_MODIFIER);
 	public final static BaseItemEconomy CROSSBOW = 				register(Items.CROSSBOW,				2f, 		1f,			0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("fletcher")).modify(CROSSBOW_ENCHANTMENT_MODIFIER);
@@ -403,7 +442,20 @@ public class EconomyFactory {
 	public final static BaseItemEconomy RED_DYE =				register(Items.RED_DYE,					0.2f, 		0.01f, 		0.001f, DIE_RANDOMNESS_FACTORY,							ImmutableSet.of("shepherd"));
 	public final static BaseItemEconomy WHITE_DYE =				register(Items.WHITE_DYE,				0.2f, 		0.01f, 		0.001f, DIE_RANDOMNESS_FACTORY,							ImmutableSet.of("shepherd"));
 	public final static BaseItemEconomy YELLOW_DYE =			register(Items.YELLOW_DYE,				0.2f, 		0.01f, 		0.001f, DIE_RANDOMNESS_FACTORY,							ImmutableSet.of("shepherd"));
-
+	
+	public final static BaseItemEconomy STONE_AXE =				register(Items.STONE_AXE,				1f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(AXE_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy STONE_PICKAXE =			register(Items.STONE_PICKAXE,			1f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(PICKAXE_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy STONE_SHOVEL =			register(Items.STONE_SHOVEL,			1f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(SHOVEL_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy STONE_HOE =				register(Items.STONE_HOE,				1f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(HOE_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy IRON_AXE =				register(Items.IRON_AXE,				20f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(AXE_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy IRON_PICKAXE =			register(Items.IRON_PICKAXE,			20f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(PICKAXE_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy IRON_SHOVEL =			register(Items.IRON_SHOVEL,				20f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(SHOVEL_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy IRON_HOE =				register(Items.IRON_HOE,				20f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(HOE_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy DIAMOND_AXE =			register(Items.DIAMOND_AXE,				40f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(AXE_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy DIAMOND_PICKAXE =		register(Items.DIAMOND_PICKAXE,			40f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(PICKAXE_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy DIAMOND_SHOVEL =		register(Items.DIAMOND_SHOVEL,			40f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(SHOVEL_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	public final static BaseItemEconomy DIAMOND_HOE =			register(Items.DIAMOND_HOE,				40f, 		1f, 		0.001f, new ConstantValue<Integer>(1),					ImmutableSet.of("toolsmith")).modify(HOE_ENCHANTMENT_MODIFIER).modify(SELL_ONLY_MODIFIER);
+	
 	
 	
 	
