ac744135c8b7a2d4892c2ed09c7a992e5567d78f
JulianWww <Julian.wandhoven@gmail.com>
2022-12-04 17:07:42 +0100

    added basics

---
+++ src/main/java/net/denanu/amazia/entities/village/client/ChefRenderer.java
@@ -0,0 +1,31 @@
+package net.denanu.amazia.entities.village.client;
+
+import net.denanu.amazia.Amazia;
+import net.denanu.amazia.entities.village.server.ChefEntity;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+public class ChefRenderer extends GeoEntityRenderer<ChefEntity> {
+	public ChefRenderer(final EntityRendererFactory.Context ctx) {
+		super(ctx, new ChefModel());
+		this.shadowRadius = 0.4f;
+	}
+
+	@Override
+	public Identifier getTextureResource(final ChefEntity instance) {
+		return new Identifier(Amazia.MOD_ID, "textures/entity/farmer_texture.png");
+	}
+
+	@Override
+	public RenderLayer getRenderType(final ChefEntity animatable, final float partialTicks, final MatrixStack stack,
+			final VertexConsumerProvider renderTypeBuffer, final VertexConsumer vertexBuilder,
+			final int packedLightIn, final Identifier textureLocation) {
+
+		return super.getRenderType(animatable, partialTicks, stack, renderTypeBuffer, vertexBuilder, packedLightIn, textureLocation);
+	}
+}
---
+++ src/main/java/net/denanu/amazia/entities/village/server/ChefEntity.java
@@ -0,0 +1,62 @@
+package net.denanu.amazia.entities.village.server;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.CraftingRecipe;
+import net.minecraft.world.World;
+import oshi.util.tuples.Triplet;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
+
+public class ChefEntity extends AmaziaVillagerEntity implements IAnimatable {
+	AnimationFactory factory = new AnimationFactory(this);
+
+	public ChefEntity(final EntityType<? extends PassiveEntity> entityType, final World world) {
+		super(entityType, world);
+	}
+
+	@Override
+	public Triplet<ItemStack, Integer, Integer> getDepositableItems() {
+		return null;
+	}
+
+	@Override
+	public HashMap<Item, ArrayList<CraftingRecipe>> getCraftables() {
+		return null;
+	}
+
+	@Override
+	public boolean canDepositItems() {
+		return false;
+	}
+
+	private <E extends IAnimatable> PlayState predicate(final AnimationEvent<E> event) {
+		if (event.isMoving()) {
+			event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.farmer.walk", true));
+			return PlayState.CONTINUE;
+		}
+
+		event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.farmer.idle", true));
+		return PlayState.CONTINUE;
+	}
+
+	@Override
+	public void registerControllers(final AnimationData data) {
+		data.addAnimationController(new AnimationController<ChefEntity>(this, "controller", 0, this::predicate));
+	}
+	@Override
+	public AnimationFactory getFactory() {
+		return this.factory;
+	}
+
+}
