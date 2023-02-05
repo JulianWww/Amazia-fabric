6db01f703a01eee4ac6713e4f3a59b5110f85aef
Julian Wandhoven <Julian.wandhoven@gmail.com>
2023-02-05 22:14:09 +0100

    Update generate.py

--- generators/token/generate.py
+++ generators/token/generate.py
@@ -12,7 +12,7 @@ path = f"https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1
 outPath = "../../src/main/resources/assets/amazia/textures/item/tokens/"
 
 images = {
-  "miner": "stone_pickaxe",
+  "miner": "iron_pickaxe",
   "bard": "music_disc_chirp",
   "chef": "cooked_beef",
   "druid": "kelp",
