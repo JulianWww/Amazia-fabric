477ff6d27906166d07ca6ff87bb0ecf9f6df0838
JulianWww/generator <generator[bot]@JulianWww.github.io>
2023-03-07 22:24:53 +0000

    auto generated data

--- src/main/java/net/denanu/amazia/particles/AmaziaClientParticles.java
+++ src/main/java/net/denanu/amazia/particles/AmaziaClientParticles.java
@@ -7,6 +7,20 @@ import net.minecraft.client.particle.SuspendParticle;
 public class AmaziaClientParticles {
 	public static void setup() {
 		// PYTHON GENERATOR BEGIN
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.AXE, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.BEETROOT, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.BEETROOT_SEEDS, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.CARROT, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.COAL, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.HOE, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.LAPIS, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.PICK_AXE, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.POTATO, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.SAPLING, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.SWORD, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.TORCH, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.WHEAT, SuspendParticle.Factory::new);
+		ParticleFactoryRegistry.getInstance().register(AmaziaParticles.WHEAT_SEEDS, SuspendParticle.Factory::new);
 		// PYTHON GENERATOR END
 	}
 }
--- src/main/java/net/denanu/amazia/particles/AmaziaParticles.java
+++ src/main/java/net/denanu/amazia/particles/AmaziaParticles.java
@@ -8,6 +8,20 @@ import net.minecraft.util.registry.Registry;
 
 public class AmaziaParticles {
 	// PYTHON GENERATOR BEGIN
+	public static final DefaultParticleType AXE = AmaziaParticles.register(FabricParticleTypes.simple(), "axe");
+	public static final DefaultParticleType BEETROOT = AmaziaParticles.register(FabricParticleTypes.simple(), "beetroot");
+	public static final DefaultParticleType BEETROOT_SEEDS = AmaziaParticles.register(FabricParticleTypes.simple(), "beetroot_seeds");
+	public static final DefaultParticleType CARROT = AmaziaParticles.register(FabricParticleTypes.simple(), "carrot");
+	public static final DefaultParticleType COAL = AmaziaParticles.register(FabricParticleTypes.simple(), "coal");
+	public static final DefaultParticleType HOE = AmaziaParticles.register(FabricParticleTypes.simple(), "hoe");
+	public static final DefaultParticleType LAPIS = AmaziaParticles.register(FabricParticleTypes.simple(), "lapis");
+	public static final DefaultParticleType PICK_AXE = AmaziaParticles.register(FabricParticleTypes.simple(), "pick_axe");
+	public static final DefaultParticleType POTATO = AmaziaParticles.register(FabricParticleTypes.simple(), "potato");
+	public static final DefaultParticleType SAPLING = AmaziaParticles.register(FabricParticleTypes.simple(), "sapling");
+	public static final DefaultParticleType SWORD = AmaziaParticles.register(FabricParticleTypes.simple(), "sword");
+	public static final DefaultParticleType TORCH = AmaziaParticles.register(FabricParticleTypes.simple(), "torch");
+	public static final DefaultParticleType WHEAT = AmaziaParticles.register(FabricParticleTypes.simple(), "wheat");
+	public static final DefaultParticleType WHEAT_SEEDS = AmaziaParticles.register(FabricParticleTypes.simple(), "wheat_seeds");
 	// PYTHON GENERATOR END
 
 
